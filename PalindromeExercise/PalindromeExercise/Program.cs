ee18a79fe2f942b1771862fd1bbca0bc731648e1
corey <coreymmartin1991@gmail.com>
2022-06-24 12:09:58 -0500

    cleaned up

--- PalindromeExercise/PalindromeExercise/Program.cs
+++ PalindromeExercise/PalindromeExercise/Program.cs
@@ -7,11 +7,6 @@ namespace PalindromeExercise
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            var smithy = new WordSmith();
-            smithy.IsAPalindrome("racecar");
-            smithy.IsAPalindrome("racecar1");
-            smithy.IsAPalindrome("race car");
-            smithy.IsAPalindrome("race car1");
         }
     }
 }
--- PalindromeExercise/PalindromeExercise/WordSmith.cs
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -7,13 +7,19 @@ namespace PalindromeExercise
 {
     public class WordSmith
     {
-        public bool IsAPalindrome(string str) {
-            if (str.Length > 0) {
+        public bool IsAPalindrome(string str) 
+        {
+            if (str.Length > 0)
+            {
                 var strTrim = str.Where(x => x != ' ').ToArray();
-                for (int i =0; i < (strTrim.Length / 2); i++) {
+                for (int i = 0; i < (strTrim.Length / 2); i++)
+                {
                     if (strTrim[i].ToString().ToUpper() != strTrim[strTrim.Length - 1 - i].ToString().ToUpper())
-                        return false; }
-                return true; }
-            return false; }
+                        return false;
+                }
+                return true;
+            }
+            return false;
+        }
     }
 }
